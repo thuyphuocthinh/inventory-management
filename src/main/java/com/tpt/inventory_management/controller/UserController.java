889c3b820bfd54a4dd138e0d743304d39e1fa291
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2025-05-04 17:04:19 +0700

    [dev] add all controllre

---
+++ src/main/java/com/tpt/inventory_management/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.tpt.inventory_management.controller;
+
+import com.tpt.inventory_management.dto.Response;
+import com.tpt.inventory_management.dto.UserDTO;
+import com.tpt.inventory_management.entity.User;
+import com.tpt.inventory_management.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+
+    private final UserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Response> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.updateUser(id, userDTO));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> deleteUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.deleteUser(id));
+    }
+
+    @GetMapping("/transactions/{userId}")
+    public ResponseEntity<Response> getUserAndTransactions(@PathVariable Long userId) {
+        return ResponseEntity.ok(userService.getUserTransactions(userId));
+    }
+
+    @GetMapping("/current")
+    public ResponseEntity<User> getCurrentUser(){
+        return ResponseEntity.ok(userService.getCurrentLoggedInUser());
+    }
+}
